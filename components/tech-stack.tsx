36013d2079a208b868507aeff2993dc6f3d9a322
qetr1ck <orestprustayko@gmail.com>
2023-04-08 23:44:39 +0300

    reduce count

--- components/tech-stack.tsx
+++ components/tech-stack.tsx
@@ -7,7 +7,7 @@ export function TechStack() {
     <div className="max-w-7xl sm:px-16 px-6 mx-auto">
       <span className="hash-span"></span>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.slice(0, 14).map((technology) => (
+        {technologies.slice(0, 12).map((technology) => (
           <div
             className="w-28 h-28 cursor-pointer"
             key={technology.name}
